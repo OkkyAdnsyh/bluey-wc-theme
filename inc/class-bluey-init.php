9d231d7578c909e78a19891c6effc88b694885d7
OkkyAdnsyh <landing.pageportofolio@gmail.com>
2024-05-08 14:03:42 +0700

    fix:after_theme_Setup to after_setup_theme

--- inc/class-bluey-init.php
+++ inc/class-bluey-init.php
@@ -13,7 +13,7 @@
              * Execute function when class created
              */
             public function __construct(){
-                add_action( 'after_theme_setup', array($this, 'setup') );
+                add_action( 'after_setup_theme', array($this, 'setup') );
 				add_action( 'after_switch_theme', array($this, 'register_new_page') );
                 add_action( 'wp_enqueue_scripts', array($this, 'register_style'), 30 );
                 add_action( 'wp_enqueue_scripts', array($this, 'register_script'), 10 );
